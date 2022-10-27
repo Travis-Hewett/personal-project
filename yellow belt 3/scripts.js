ee4616918476678d11fbe2917eb52dfa642cd449
Travis Hewett <102689519+Travis-Hewett@users.noreply.github.com>
2022-10-27 07:47:26 -0500

    Add files via upload

---
+++ yellow belt 3/scripts.js
@@ -0,0 +1,48 @@
+
+
+// this is for the audio 
+let audio = new Audio();
+audio.src = "./assets/poke.mp3"
+console.log(audio);
+// 
+
+// these are all failed js attempts to expand the options
+
+// text.onmouseover=function(){
+//     text.style.display = "none";
+// }
+
+// one.onmouseout=function() {
+
+//     text.style.display='none'
+// }
+
+// let x = document.getElementById('one');
+
+// function expand() {
+//     document.getElementById('one').innerText=".moretext"; 
+
+// }
+
+// function back() {
+//     document.getElementById('one').innerText=back
+// }
+
+
+// function expand() {
+//     document.getElementById('two').innerText="somethingelse" 
+
+// }
+
+// function back() {
+//     document.getElementById('two').innerText
+// }
+
+// function expand() {
+//     document.getElementById('three').innerText="somethingelse" 
+
+// }
+
+// function back() {
+//     document.getElementById('three').innerText
+// }
